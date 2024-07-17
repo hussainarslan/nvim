db5f785ba59e6686e9b49717257c356221f532c3
hussainarslan <hussain.chawla@gmail.com>
2024-07-17 16:53:18 +0500

    feat: restructured to use lazy instead of packer

--- lua/hussainarslan/lazy_init.lua
+++ lua/hussainarslan/lazy_init.lua
@@ -12,6 +12,52 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "hussainarslan.lazy",
-    change_detection = { notify = false }
+    spec = 'hussainarslan.lazy',
+    change_detection = { notify = false}
 })
+--require("lazy").setup({
+--    'folke/tokyonight.nvim',
+--    'nvim-lua/plenary.nvim',
+--    'nvim-telescope/telescope.nvim',
+--
+--    {
+--        'rose-pine/neovim',
+--        name = 'rose-pine',
+--        config = function()
+--            vim.cmd('colorscheme rose-pine')
+--        end
+--    },
+--    {
+--        'folke/trouble.nvim',
+--        config = function()
+--            require('trouble').setup {
+--                icons = false,
+--            }
+--        end
+--    },
+--    {
+--        'nvim-treesitter/nvim-treesitter',
+--        build = ':TSUpdate',
+--    },
+--  'nvim-treesitter/playground',
+--  'theprimeagen/harpoon',
+--  'mbbill/undotree',
+--  'tpope/vim-fugitive',
+--  --[[
+--  use {
+--	  "neovim/nvim-lspconfig",
+--	  requires = {
+--		  {"williamboman/mason.nvim"},
+--		  {"williamboman/mason-lspconfig.nvim"},
+--		  {"hrsh7th/cmp-nvim-lsp"},
+--		  {"hrsh7th/cmp-buffer"},
+--		  {"hrsh7th/cmp-path"},
+--		  {"hrsh7th/cmp-cmdline"},
+--		  {"hrsh7th/nvim-cmp"},
+--		  {"L3MON4D3/LuaSnip"},
+--		  {"saadparwaiz1/cmp_luasnip"},
+--		  {"j-hui/fidget.nvim"},
+--	  },
+--  }
+--  ]]--
+-- }, {})
